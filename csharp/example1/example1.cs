e7f219cb85cad7e93b6ca57f00cb340513eebdee
David Days <david.days@maplarge.com>
2020-12-03 08:25:25 -0500

    expanded capabilities and modified .gitignore

--- csharp/example1/example1.cs
+++ csharp/example1/example1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Collections;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
@@ -9,7 +10,9 @@ using System.Text;
 using System.Security.Cryptography.X509Certificates;
 using System.IO;
 using System.IO.Compression;
-using System.Text.Json;             // requires .NET Core 3.0 or higher, or the NuGet package
+using System.Linq;
+using System.Text.Json;
+using Newtonsoft.Json.Linq; // requires .NET Core 3.0 or higher, or the NuGet package
 
 namespace SSLClient
 {
@@ -65,8 +68,9 @@ namespace SSLClient
         public static String username = "XXXXXXXX";
         public static String apikey = "XXXXXXXXXXXXXXXXXXXX";
         public static Boolean useCompression = true;
-        public static String initiation_command = "live username " + username + " password " + apikey +
-                (useCompression ? " compression deflate" : "") + "\n";
+        public static string eventsList = null;
+        public static List<Dictionary<string,string>> records = new List<Dictionary<string, string>>();
+        public static String initiation_command => $"live username {username} password {apikey} {(useCompression ? " compression deflate" : "")} optype_filter ga";
 
         // The following method is invoked by the RemoteCertificateValidationDelegate
         // prevent communication with unauthenticated server
@@ -122,7 +126,12 @@ namespace SSLClient
             // Send initiation command to the server.
             // Encode to a byte array.
             Console.WriteLine(initiation_command);
-            byte[] messsage = Encoding.UTF8.GetBytes(initiation_command + "\n");
+            string eventsCommand = "";
+            if (!string.IsNullOrEmpty(eventsList) && !string.IsNullOrWhiteSpace(eventsList)) {
+                Console.WriteLine("requesting the following event info:  "+eventsList);
+                eventsCommand = $" events \"{eventsList}\"";
+            }
+            byte[] messsage = Encoding.UTF8.GetBytes(initiation_command + eventsCommand+ "\n");
             sslStream.Write(messsage);
             sslStream.Flush();
 
@@ -168,24 +177,108 @@ namespace SSLClient
             Console.WriteLine("Client closed.");
         }
 
-        public static void parse(string mes)
-        {
-            FlightObject flight = JsonSerializer.Deserialize<FlightObject>(mes);
-            Console.WriteLine(" --------------- Message ------------------ \n");
-            Console.WriteLine(flight.toString());
-            Console.WriteLine(" ------------------------------------------ \n");
+        public static void parse(string mes) {
+            JObject mJson = JObject.Parse(mes);
+            Dictionary<string,string> record = new Dictionary<string, string>();
+            foreach (var mkey in mJson.Properties()) {
+                if (mkey.Name.Equals("waypoints", StringComparison.InvariantCultureIgnoreCase)) {
+                    //convert to WKT
+                    var wptArr = mkey.Value as JArray;
+                    int wptCount = wptArr.Count;
+                    string wktType = wptCount > 1 ? "\"LINESTRING" : "\"POINT";
+                    StringBuilder wktBuilder = new StringBuilder(wktType);
+                    wktBuilder.Append("(");
+                    for (int i = 0; i < wptCount; i++) {
+                        var llObj = wptArr[i];
+                        string lngTxt = String.Empty;
+                        string latTxt = String.Empty;
+                        if (llObj["lon"] != null) lngTxt = llObj["lon"].ToString();
+                        if (llObj["lat"] != null) latTxt = llObj["lat"].ToString();
+                        wktBuilder.AppendFormat("{0} {1}", lngTxt, latTxt);
+                        if (i < wptCount - 1) wktBuilder.Append(",");
+                    }
+                    wktBuilder.Append(")\"");
+                    record.Add("zzwkt", wktBuilder.ToString());
+                } else {
+                    record.Add(mkey.Name, mkey.Value.ToString());
+                }
+            }
+
+            if (record.ContainsKey("lat") && record.ContainsKey("lon") && !record.ContainsKey("flightwkt")) {
+                record.Add("zzwkt", $"\"POINT({record["lon"]} {record["lat"]})\"");
+            }
+            records.Add(record);
+        }
+
+        public static void WriteRecords(List<Dictionary<string, string>> records, string fname) {
+            StringBuilder sb = new StringBuilder();
+            List<string> columns = new List<string>();
+            foreach (var rec in records) {
+                foreach(string rkey in rec.Keys) columns.Add(rkey);
+            }
+
+            string[] columnArr = columns.Distinct().OrderBy(s => s).ToArray();
+            using (FileStream of = new FileStream(fname, FileMode.Create)) {
+                StringBuilder hdrBuilder = new StringBuilder();
+                int lastCol = columnArr.Length - 1;
+                for (int i = 0; i < columnArr.Length; i++) {
+                    hdrBuilder.Append(columnArr[i]);
+                    if (i < lastCol) {
+                        hdrBuilder.Append(",");
+                    } else {
+                        hdrBuilder.AppendLine();
+                    }
+                }
+
+                of.Write(Encoding.UTF8.GetBytes(hdrBuilder.ToString()));
+                of.Flush(true);
+                StringBuilder recordBuilder = new StringBuilder();
+                foreach (var record in records) {
+                    for (int i = 0; i < columnArr.Length; i++) {
+                        string col = columnArr[i];
+                        if (record.ContainsKey(col)) {
+                            recordBuilder.Append(record[col]);
+                        } else {
+                            recordBuilder.Append(String.Empty);
+                        }
+
+                        if (i < lastCol) {
+                            recordBuilder.Append(",");
+                        } else {
+                            recordBuilder.AppendLine();
+                        }
+                    }
+                }
+                of.Write(Encoding.UTF8.GetBytes(recordBuilder.ToString()));
+                of.Flush(true);
+            }
         }
 
         public static int Main(string[] args)
         {
 
+            if (args == null || args.Length < 2) throw new Exception("[main] username apikey [events list]");
+            string eventsListing = null;
+            if (args.Length > 2) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 2; i < args.Length; i++) {
+                    sb.Append(args[i]);
+                    if (i < args.Length - 1) sb.Append(" ");
+                }
+
+                eventsListing = sb.ToString();
+            }
             // machineName is the host running the server application.
             String machineName = "firehose.flightaware.com";
             String serverCertificateName = machineName;
 
+            SSLClient.username = args[0];
+            SSLClient.apikey = args[1];
+            SSLClient.eventsList = eventsListing;
             //connect, read data
             SSLClient.RunClient(machineName, serverCertificateName);
 
+            WriteRecords(SSLClient.records, $"{eventsListing.Replace(" ","")}.csv");
             Console.WriteLine(" Hit Enter to end ...");
             Console.Read();
             return 0;
